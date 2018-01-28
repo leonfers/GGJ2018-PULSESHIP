fd120b3d8501b2e807a86f44c6a853c4c55a7efb
Leonardo Salmento <Salmento_@hotmail.com>
2018-01-28 13:39:08 -0300

    algo

--- Assets/destroy_by_contact.cs
+++ Assets/destroy_by_contact.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class destroy_by_contact : MonoBehaviour {
 
 
 	void OnTriggerEnter(Collider other) 
 	{
-		if (other.tag=="Player" || other.tag == "bolt")
+		if (other.tag=="Player" )
 		{
 			Destroy (other.gameObject);
-			var health = gameObject.GetComponent<Health> ();
-			health.health = health.health -1;
+            SceneManager.LoadScene("Menu");
 		}
+        if (other.tag == "bolt")
+        {
+            Destroy(other.gameObject);
+            var health = gameObject.GetComponent<Health>();
+            health.health = health.health - 1;
+        }
+        
 
 	}
 
